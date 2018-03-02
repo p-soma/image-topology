4219aa513c250cfb2276dc71c38ffc903f1ec711
Paul Soma <somapaul@cse.msu.edu>
2018-03-02 08:56:24 -0500

    remove I/O in function readIML

--- select_patches/readIML.m
+++ select_patches/readIML.m
@@ -2,7 +2,6 @@ function buf = readIML(filename)
 % function to read an iml image file into a matrix of pixel intensities
 %   param filename string the name of the file to be read
 %   returns buf, the matrix of greyscale pixel intensities from the image
-    filename
     f1 = fopen(filename,'rb','ieee-be');
     w=1536;h=1024;
     buf=fread(f1,[w,h],'uint16');
