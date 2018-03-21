a298edf09a4a6d9d00d8da515b160ac9e4ff1d4e
Paul Soma <somapaul@cse.msu.edu>
2018-03-21 03:23:25 -0400

    code to save fiber in extractFiber

--- lib/extractFiber.m
+++ lib/extractFiber.m
@@ -1,9 +1,10 @@
-function fiber = extractFiber(baseSpace, totalSpace, p, patch)
+function fiber = extractFiber(baseSpace, totalSpace, p, patch, path)
 % function to extract a fiber in MN over a patch in Mn
 %   param baseSpace patch space with dimension n^2
 %   param totalSpace patch space with dimension N^2 where N = n^2
 %   param p percent of nearest neighbors to extract around patch in M_n
 %   param patch the patch vector over which the fiber is extracted
+%   param path to save the fiber
 
     % compute number of nearest neighbors k based on cut param p %
     nPoints = size(baseSpace,1);
@@ -14,4 +15,5 @@ function fiber = extractFiber(baseSpace, totalSpace, p, patch)
     
     fiber = totalSpace(Idx,:);
 
+    csvwrite(path,fiber);
 end
