b6b32752dcabb027adeba10e62ca4ce628a7cf86
Paul Soma <somapaul@cse.msu.edu>
2018-04-10 06:06:45 -0400

    only addpaths in init now

--- init.m
+++ init.m
@@ -4,8 +4,8 @@ addpath('/mnt/home/somapaul/cmse/van_hateren_study/software/image-topology/lib/f
 addpath('/mnt/home/somapaul/cmse/van_hateren_study/software/image-topology/lib/preprocess');
 addpath('/mnt/home/somapaul/cmse/van_hateren_study/software/image-topology/misc/');
 
-[lam, A] = DCTBasis();
-E = A';
+%[lam, A] = DCTBasis();
+%E = A';
 
 %M9 = dlmread('/mnt/home/somapaul/cmse/van_hateren_study/data/patches/M_9/spaces/M_9_patches.txt',',');
 %M9_map_P3 = dlmread('/mnt/home/somapaul/cmse/van_hateren_study/data/patches/M_9/spaces/M_9_as_3by3.txt',',');
