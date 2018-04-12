a2d8f57d91d458db47ed2d26c80fac3147f7c92e
Paul Soma <somapaul@cse.msu.edu>
2018-04-12 08:43:07 -0400

    output var name change

--- loadM9.m
+++ loadM9.m
@@ -1,3 +1,3 @@
 % loadM9
 M9_path = '/mnt/home/somapaul/cmse/van_hateren_study/data/patches/M_9/';
-M3 = dlmread('/mnt/home/somapaul/cmse/van_hateren_study/data/patches/M_9/spaces/M_9_patches.txt',',');
+M9 = dlmread('/mnt/home/somapaul/cmse/van_hateren_study/data/patches/M_9/spaces/M_9_patches.txt',',');
