7e8f0fdc786f5a3b4996606913ff12d5a7416433
Paul Soma <somapaul@cse.msu.edu>
2018-07-12 20:47:23 -0400

    100K neighbors

--- lib/fibers/blobKnn.m
+++ lib/fibers/blobKnn.m
@@ -14,7 +14,7 @@ target = reshape(target,1,81);
 patches = dlmread('/mnt/home/somapaul/cmse/van_hateren_study/data/patches/M9/M9_log_meansub_dNormalize',',');
 
 
-[Idx,D] = knnsearch(patches,target);
+[Idx,D] = knnsearch(patches,target,'K',100000);
 
 dlmwrite('gaussblob_sd1_m0_IDX.txt',Idx,',');
 dlmwrite('gaussblob_sd1_m0_dists.txt',D,',');
