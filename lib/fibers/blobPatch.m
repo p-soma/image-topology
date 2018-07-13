720af7b9db8e682aa0b3482ca1ec13a4c97b50d9
Paul Soma <somapaul@cse.msu.edu>
2018-07-13 05:24:19 -0400

    add dnormalization

--- lib/fibers/blobPatch.m
+++ lib/fibers/blobPatch.m
@@ -14,10 +14,14 @@ function patch = blobPatch(sdx,mx,sdy,my);
 
     patch = reshape(patch,1,(dim^2));
     patch = patch - mean(patch,2);
-    patch = (patch + 1) * (4/5);
-    patch = reshape(patch,dim,dim);
-    figure;
-    subplot(1,2,1);
-    imshow(patch)
+    dNorm = getPatchDNorms(patch);
+    patch = patch / dNorm;
+    
+    
+    % patchdisp = (patch + 1) * (4/5);
+    % patchdisp = reshape(patchdisp,dim,dim);
+    % figure;
+    % subplot(1,2,1);
+    % imshow(patchdisp)
     
 end
