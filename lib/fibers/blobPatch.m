790bae6065d82ed15f7eb11452ea5e5a6a55472b
Paul Soma <somapaul@cse.msu.edu>
2018-07-12 20:24:34 -0400

    suppress output in func

--- lib/fibers/blobPatch.m
+++ lib/fibers/blobPatch.m
@@ -2,20 +2,20 @@ function patch = blobPatch(sdx,mx,sdy,my);
     lim = 1;
     dim = 9;
     
-    x = linspace(-lim,lim,dim)
-    X = gaussmf(x,[sdx,mx])
+    x = linspace(-lim,lim,dim);
+    X = gaussmf(x,[sdx,mx]);
     %X = cos(x);
-    y = linspace(-lim,lim,dim)
-    Y = gaussmf(y,[sdy,my])
+    y = linspace(-lim,lim,dim);
+    Y = gaussmf(y,[sdy,my]);
     %Y = cos(y);
-    [a,b] = meshgrid(X,Y)
+    [a,b] = meshgrid(X,Y);
 
-    patch = a+b
+    patch = a+b;
 
-    patch = reshape(patch,1,(dim^2))
-    patch = patch - mean(patch,2)
-    patch = (patch + 1) * (4/5)
-    patch = reshape(patch,dim,dim)
+    patch = reshape(patch,1,(dim^2));
+    patch = patch - mean(patch,2);
+    patch = (patch + 1) * (4/5);
+    patch = reshape(patch,dim,dim);
     figure;
     subplot(1,2,1);
     imshow(patch)
