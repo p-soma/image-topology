b8e1073e712a28468e823dc0fea175609bdd8b11
Paul Soma <somapaul@cse.msu.edu>
2018-07-23 08:25:05 -0400

    ready for hpcc

--- lib/fibers/sublevelSetPersistence.m
+++ lib/fibers/sublevelSetPersistence.m
@@ -1,13 +1,13 @@
 % sublevel set persistence 
-
+addpath('/mnt/home/somapaul/cmse/van_hateren_study/software/image-topology/lib/fibers')
 cd /mnt/home/somapaul/cmse/van_hateren_study/software/3TDATools
 init;
 
-I = 24;
-J = 16;
+ii = 24;
+jj = 16;
 type = 'log_prior';
 
-fib = loadFiber(I,J,type);
+fib = loadFiber(ii,jj,type);
 
 pts = fib.fine_images;
 
@@ -29,14 +29,15 @@ for i=1:numpts
     for j=(i+1):numpts
         pi = pts(i,:);
         pj = pts(j,:);
-        val = max(dens(i), dens(j), sqrt(sum((pi-pj).^2)));
+        val = max([dens(i), dens(j), sqrt(sum((pi-pj).^2))]);
         S = [S;[i-1, j-1, val]];
     end
 end
 
 [I , J] = rca1mfscm(S,max(dens));
 
-fname = strcat('slsp_',num2str(I),'_',num2str(J),'_',datestr(now,'mmddTHHMMSS'));
+fname = strcat('slsp_',num2str(ii),'_',num2str(jj),'_',datestr(now,'mmddTHHMMSS'));
 
+path = fullfile('/mnt/home/somapaul/cmse/van_hateren_study/software/slsp',fname);
 
-
+dlmwrite(fname,J);
